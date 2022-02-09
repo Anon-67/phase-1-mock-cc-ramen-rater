c2051d7f0668decd1a6404f3e6760a6e2cf22288
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-09 13:36:36 -0600

    finished deliverables

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@
 
 
 window.addEventListener("DOMContentLoaded", () => {
+    initialize();
+
+})
+
+function initialize() {
     fetch("http://localhost:3000/ramens")
         .then(resp => resp.json())
         .then(resp2 => {
@@ -16,60 +21,57 @@ window.addEventListener("DOMContentLoaded", () => {
     let imgContainer = document.getElementById("ramen-menu")
     function renderImg(param){
         const img = document.createElement("img");
-        const details = document.getElementById("ramen-detail");
-        img.id = "test"
         img.src = param.image;
         imgContainer.appendChild(img);
-    }
 
-        //Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
-        
-        imgContainer.addEventListener("click", () => testFunction(param));
+        img.addEventListener("click", () => testFunction(param));
         function testFunction(e) {
-            console.log(e.id)
-            // fetch(`http://localhost:3000/ramens?id=${e.id}`
-            //     .then(resp1 => resp1.json)
-            //     .then(resp2 = () => {
-            //         let name = document.querySelector(".name")
-            //         name.innerHTML = console.log(e.target);
+            console.log(e)
 
-            //         let photo = document.querySelector(".detail-image");
-            //         photo.src = "./assets/ramen/shoyu.jpg"
+            let name = document.querySelector(".name")
+            name.innerHTML = e.name;
 
-            //         document.querySelector(".restaurant").innerHTML = "dude"
-            //     }))
+            let photo = document.querySelector(".detail-image");
+            photo.src = e.image
+
+            document.querySelector(".restaurant").innerHTML = e.restaurant
+                
 
         }
     }
+}
 
 
-    let form = document.getElementById("new-ramen");
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        let name = document.getElementById("new-name").value
-        let restaurant = document.getElementById("new-restaurant").value
-        let image = document.getElementById("new-image").value
-        let rating = document.getElementById("new-rating")
-        let comment = document.getElementById("new-comment")
+let form = document.getElementById("new-ramen");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let name = document.getElementById("new-name").value
+    let restaurant = document.getElementById("new-restaurant").value
+    let image = document.getElementById("new-image").value
+    let rating = document.getElementById("new-rating")
+    let comment = document.getElementById("new-comment")
 
-        let data = {
-            "id": 6,
-            "name": `${name}`,
-            "restaurant": `${restaurant}`,
-            "image": `${image}`,
-            "rating": `${rating}`,
-            "comment": `${comment}`
-          }
+    let data = {
+        "id": 6,
+        "name": `${name}`,
+        "restaurant": `${restaurant}`,
+        "image": `${image}`,
+        "rating": `${rating}`,
+        "comment": `${comment}`
+        }
 
-        fetch("http://localhost:3000/ramens", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
+
+    fetch("http://localhost:3000/ramens", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    
+    initialize();
         
 
     })
     
-})
+
